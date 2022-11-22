2f2b0242aaecd8dbfe31e8b3ba2eba6d1a26c712
threepat1 <42788704+threepat1@users.noreply.github.com>
2022-11-21 22:58:15 -0500

    modules login logout and nav

--- server/config/app.js
+++ server/config/app.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 let createError = require('http-errors');
 let express = require('express');
 let path = require('path');
@@ -6,6 +7,63 @@ let logger = require('morgan');
 
 let indexRouter = require('../routes/index');
 let usersRouter = require('../routes/users');
+=======
+// installed 3rd party packages
+let createError = require("http-errors");
+let express = require("express");
+let path = require("path");
+let cookieParser = require("cookie-parser");
+let logger = require("morgan");
+let cors = require("cors");
+
+//modules for authentication
+let session = require("express-session");
+let passport = require("passport");
+
+let passportJWT = require("passport-jwt");
+let JWTStrategy = passportJWT.Strategy;
+let ExtractJWT = passportJWT.ExtractJwt;
+
+let passportLocal = require("passport-local");
+let localStratergy = passportLocal.Strategy;
+let flash = require("connect-flash");
+
+//database_setup
+let mongoose = require("mongoose");
+let DB = require("./db");
+
+//point mongoose to the DB URI
+mongoose.connect(DB.URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+let mongodb = mongoose.connection;
+mongodb.on("error", console.error.bind(console, "connection error:"));
+mongodb.once("open", () => {
+  console.log("Database Connected");
+});
+
+
+let jwtOptions = {};
+jwtOptions.jwtFromRequest = ExtractJWT.fromAuthHeaderAsBearerToken();
+jwtOptions.secretOrKey = DB.Secret;
+//control how token is exacted 
+let strategy = new JWTStrategy(jwtOptions, (jwt_payload, done) => {
+  //find user by id
+  User.findById(jwt_payload.id)
+    .then((user) => {
+      //refresh user
+      return done(null, user);
+    })
+    .catch((err) => {
+      return done(null, false);
+    });
+});
+
+passport.use(strategy);
+
+let indexRouter = require("../routes/index");
+let usersRouter = require("../routes/users");
+let bracketRouter = require("../routes/bracket");
+>>>>>>> Stashed changes
 
 let app = express();
 
@@ -19,8 +77,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+<<<<<<< Updated upstream
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+=======
+//jquery nodejs
+var jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const { window } = new JSDOM();
+const { document } = new JSDOM("").window;
+global.document = document;
+var $ = require("jquery")(window);
+>>>>>>> Stashed changes
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ server/controllers/bracket.js
@@ -0,0 +1,120 @@
+let express = require("express");
+let router = express.Router();
+let mongoose = require("mongoose");
+
+//create reference to the model (dbschema )
+let Bracket = require("../models/bracket");
+
+//display tournament info
+module.exports.displayBracketList = (req, res, next) => {
+  Bracket.find((err, bracketList) => {
+    if (err) {
+      return console.error(err);
+    } else {
+      res.render("bracket/teamlist", {
+        title: "Bracket",
+        BracketList: bracketList,
+        displayName: req.user ? req.user.displayName : "",
+      });
+      //render bracket.ejs and pass title and Bracketlist variable we are passing bracketList object to BracketList property
+    }
+  });
+};
+
+module.exports.addpage = (req, res, next) => {
+  res.render("bracket/createPage", { title: "Add TeamBracket", displayName: req.user ? req.user.displayName : "",});
+};
+
+module.exports.addprocesspage = (req, res, next) => {
+  let newbracket = Bracket({
+    tournamentName: req.body.tournamentName,
+    gameType: req.body.gameType,
+    players: req.body.players,
+    description: req.body.description,
+    teams: req.body.teams,
+  });
+
+  Bracket.create(newbracket, (err, Bracket) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    } else {
+      res.redirect("/bracket-list");
+      console.log(req.body.teams);
+    }
+  });
+};
+
+//show display for add player
+module.exports.addPlayerpage = async (req, res, next) => {
+  let id = req.params.id; //id of actual object
+
+  Bracket.findById(id, (err, bracketoshow) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    } else {
+      //show the edit view
+      res.render("bracket/list", {
+        title: "Tournament Bracket",
+        bracket: bracketoshow,
+      });
+      console.log(bracketoshow);
+    }
+  });
+};
+
+module.exports.displayeditpage = (req, res, next) => {
+  let id = req.params.id; //id of actual object
+
+  Bracket.findById(id, (err, bracketoedit) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    } else {
+      //show the edit view
+      res.render("bracket/edit", {
+        title: "Edit Bracket",
+        bracket: bracketoedit,
+        displayName: req.user ? req.user.displayName : "",
+      });
+    }
+  });
+};
+
+module.exports.processingeditpage = (req, res, next) => {
+  let id = req.params.id; //id of actual object
+
+  let updatebracket = Bracket({
+    _id: id,
+    tournamentName: req.body.tournamentName,
+    gameType: req.body.gameType,
+    players: req.body.players,
+    description: req.body.description,
+    teams: req.body.teams,
+  });
+  Bracket.updateOne({ _id: id }, updatebracket, (err) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    } else {
+      //refresh the bracket list
+      res.redirect("/bracket-list");
+    }
+  });
+};
+
+module.exports.deletepage = (req, res, next) => {
+  let id = req.params.id;
+  Bracket.deleteOne({ _id: id }, (err) => {
+    //remove
+    if (err) {
+      console.log(err);
+      res.end(err);
+    } else {
+      //refresh bracket list
+      res.redirect("/bracket-list");
+    }
+  });
+  //res.redirect("/bracket-list");
+};
