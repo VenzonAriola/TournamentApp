0aaa6b21735363aa486aa86b9f210c4c19c558a6
mlagmay311 <mlagmay@my.centennialcollege.ca>
2022-11-24 13:34:19 -0800

    Updated indexjs

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,27 +1,27 @@
 let express = require("express");
 let router = express.Router();
 
-let indexController = require("../controllers/index");
+let bracketController = require("../controllers/index");
 
 /* GET home page. */
-router.get("/", indexController.displayHomepage);
+router.get("/", bracketController.displayHomepage);
 
 /* GET home page. */
-router.get("/Tourney Team", indexController.displayHomepage);
+router.get("/Tourney Team", bracketController.displayHomepage);
 
 /* GET Route for displaying the Login page */
-router.get("/login", indexController.displayLoginPage);
+router.get("/login", bracketController.displayLoginPage);
 
 /* POST Route for processing the Login page */
-router.post("/login", indexController.processLoginPage);
+router.post("/login", bracketController.processLoginPage);
 
 /* GET Route for displaying the Register page */
-router.get("/register", indexController.displayRegisterPage);
+router.get("/register", bracketController.displayRegisterPage);
 
 /* POST Route for processing the Register page */
-router.post("/register", indexController.processRegisterPage);
+router.post("/register", bracketController.processRegisterPage);
 
 /* GET to perform UserLogout */
-router.get("/logout", indexController.performLogout);
+router.get("/logout", bracketController.performLogout);
 
 module.exports = router;
